aa08aaf9e1b64a197c2aa8db18352a4c806d335d
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-09 02:55:55 -0400

    list of items for rendering

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -2,6 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'http://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
+
 class App extends Component {
   render() {
     var helloWorld = 'Welcome to the Road to React';

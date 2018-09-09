0c8ad0c55587acc524a62bb4b414831a429b39e8
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-09 02:20:59 -0400

    declaring more variable and object

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -5,9 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     var helloWorld = 'Welcome to the Road to React';
+    var learn = 'Super excited to learn React';
+    var userName = {
+      firstname: 'Oliver',
+      lastname: 'Gomes'
+    }
     return (
       <div className="App">
         <h2>{helloWorld}</h2>
+        <h3>{learn}</h3>
+        <h4>{`My name is ${userName.firstname} ${userName.lastname}`}</h4>
       </div>
     );
   }

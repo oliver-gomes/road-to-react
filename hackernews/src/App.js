11d3016e7855be60ee768a8ee925702fb469b31b
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-10 20:38:40 -0400

    button added

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -24,21 +24,23 @@ const list = [
 
 class App extends Component {
   render() {
-
     return (
       <div className="App">
-        {list.map(item => {
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          );
-        })}
+        {list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button onClick={() => this.onDismiss(item.objectID)} type='button'>
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
       </div>
     );
   }

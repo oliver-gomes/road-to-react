9ca4c8dc043147bd87e74f5d49c50e01be47c600
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-12 20:35:17 -0400

    Increment applied

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  Increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button onClick={this.Increment}>Increment</button>
+        {this.state.count}
       </div>
     );
   }

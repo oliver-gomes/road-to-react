738aaa549dff66028baa49e7c01bf8743f60f701
Oliver-Gomes <ogomesphoto@gmail.com>
2018-09-12 20:50:32 -0400

    implementing todo functionality

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -5,19 +5,39 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      count: 0
+      todos: [],
+      currentTodo: ""
     };
   }
 
-  Increment = () => {
-    this.setState({ count: this.state.count + 1 });
+  onInputChange = e => {
+    this.setState({ currentTodo: e.target.value });
   };
 
+  addTodo = () => {
+    let todosCopy = this.state.todos.slice();
+    todosCopy.push(this.state.currentTodo);
+
+    this.setState({ todos: todosCopy, currentTodo: "" });
+  };
   render() {
+    let bulletTodo = this.state.todos.map((e, i) => {
+      return <li key={i}>{e}</li>;
+    });
     return (
       <div>
-        <button onClick={this.Increment}>Increment</button>
-        {this.state.count}
+        <input
+          placeholder="Enter todo"
+          value={this.state.currentTodo}
+          onChange={this.onInputChange}
+        />
+        <button onClick={this.addTodo}>Add!</button>
+        <br />
+        {this.state.todos.length === 0 ? (
+          "No todos yet!"
+        ) : (
+          <ul>{bulletTodo}</ul>
+        )}
       </div>
     );
   }
